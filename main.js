85c1e5108766b24cac8fb0d243242a9eaf528254
Lawrence Lee <lawrence.lee@foxmail.com>
2017-07-25 16:45:42 +0800

    add target image display

--- main.js
+++ main.js
@@ -5,12 +5,30 @@ var containerCanvas = document.createElement('canvas')
 var containerContext = containerCanvas.getContext('2d')
 var targetCanvas = document.getElementById('target-cvs')
 var targetContext = targetCanvas.getContext('2d')
+
 image.onload = function () {
     sourceCanvas.width = image.width > 100 ? image.width : 100
     sourceCanvas.height = image.height > 100 ? image.height: 100
+    targetCanvas.width = 100
+    targetCanvas.height = 100
     sourceContext.drawImage(image, 0, 0, image.width, image.height)
     cropImage(0, 0, 100, 100)
 }
+function cropImage (sx, sy, ex, ey) {    
+    var dataImage = sourceContext.getImageData(sx, sy, ex, ey)
+    /* container canvas is used to transform canvas to image file. It won't be shown on page. */
+    containerCanvas.width = 100
+    containerCanvas.height = 100
+    containerContext.putImageData(dataImage, 0, 0, 0, 0, 100, 100)
+    var croppedImage = containerCanvas.toDataURL('image/png')
+    var targetImage = new Image()
+    targetImage.onload = function () {
+        targetContext.drawImage(targetImage, 0, 0, 100, 100)
+    }
+    targetImage.src = croppedImage
+}
+
+// Emitter
 document.getElementById('file-selector').addEventListener("change", function (e) {
     var file = e.target.files[0]
     var reader = new FileReader()
@@ -22,11 +40,4 @@ document.getElementById('file-selector').addEventListener("change", function (e)
 })
 function setImageURL(url) {
     image.src = url
-}
-function cropImage (sx, sy, ex, ey) {    
-    var dataImage = sourceContext.getImageData(sx, sy, ex, ey)
-    /* container canvas is used to transform canvas to image file. It won't be shown on page. */
-    containerCanvas.width = 100
-    containerCanvas.height = 100
-    containerContext.putImageData(dataImage, 0, 0, 0, 0, 100, 100)
 }
